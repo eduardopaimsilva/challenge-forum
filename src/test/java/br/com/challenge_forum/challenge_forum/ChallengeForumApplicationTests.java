5ed2756d61a86b3b4796ebc4d1d0eb708d3a8aaa
eduardo paim silva <edups.ww.com@hotmail.com>
2024-06-18 17:18:03 -0300

    challeng-forum

---
+++ src/test/java/br/com/challenge_forum/challenge_forum/ChallengeForumApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.challenge_forum.challenge_forum;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChallengeForumApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
